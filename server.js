4f635239abd35f23cda9440f2b1aeaf8d700c20e
Taras Tarlov <ttarlov@gmail.com>
2020-06-26 08:07:41 -0600

    Implement GET, PATCH, DELETE routes

---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pet Box';
+
+app.get('/', (request, response) => {
+    response.send('Oh hey Pet Box');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+
+app.get('/api/v1/pets/:id', (request, response) => {
+    const { id } = request.params;
+    const pet = app.locals.pets.find(pet => pet.id === id);
+    if (!pet) {
+    return response.sendStatus(404);
+}
+
+response.status(200).json(pet);
+});
+
+
+app.locals.pets = [
+    { id: 'a1', name: 'Rover', type: 'dog' },
+    { id: 'b2', name: 'Marcus Aurelius', type: 'parakeet' },
+    { id: 'c3', name: 'Craisins', type: 'cat' }
+];
+
+app.get('/api/v1/pets', (request, response) => {
+const pets = app.locals.pets;
+
+response.json({ pets });
+});
+
+
+
+app.post('/api/v1/pets', (request, response) => {
+const id = Date.now();
+const pet = request.body;
+
+for (let requiredParameter of ['name', 'type']) {
+if (!pet[requiredParameter]) {
+    return response
+    .status(422)
+    .send({ error: `Expected format: { name: <String>, type: <String> }. You're missing a "${requiredParameter}" property.` });
+}
+}
+
+const { name, type } = pet;
+app.locals.pets.push({ name, type, id });
+response.status(201).json({ name, type, id });
+});
+
+const deletePet = (id) => {
+    app.locals.pets = app.locals.pets.filter(pet => {
+        return pet.id !== id
+    })
+}
+
+
+app.patch('/api/v1/pets/:id', (request, response) => {
+    const { id } = request.params
+    
+    const petToChange = app.locals.pets.find(pet => pet.id === id) 
+
+    if(!petToChange) {
+        return response
+        .status(404)
+        .send({error: `No Pet Found with an id of ${id}`})
+    }else {
+        const newPet = 
+        { 
+            id: petToChange.id, 
+            name: request.body.name, 
+            type: petToChange.type
+        }
+        
+        deletePet(id)
+        app.locals.pets.push(newPet)
+        console.log(app.locals.pets)
+        return response
+        .status(202)
+        .json({newPet})
+    }
+
+});
+
+app.delete('/api/v1/pets', (request, response) => {
+    console.log(request.body.id)
+    // const { id } = request.params
+    
+    const petToDelete = app.locals.pets.find(pet => pet.id === request.body.id)
+
+    if(!petToDelete) {
+        return response
+        .status(404)
+        .send({error: `No Pet Found with an id of ${id}`})
+    } else {
+        deletePet(request.body.id)
+        return response
+        .status(202)
+        .json(app.locals.pets)
+    }
+
+})
